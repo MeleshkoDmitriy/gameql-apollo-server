5a06f17d99d703effafd14b8df765c9fe206af5b
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-11-17 19:43:50 +0300

    express added

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+import express from 'express';
 import { ApolloServer } from '@apollo/server';
-import { createHandler } from '@apollo/server/micro';
-import { microCors } from 'micro-cors';
+import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs } from './schema.js';
 import { resolvers } from './resolvers.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { constants } from './constants.js';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const startServer = async () => {
-  const cors = microCors();
-  const handler = createHandler({ apolloServer: server });
+await server.start();
 
-  return cors(handler);
-};
+app.use('/api/graphql', expressMiddleware(server, {
+  cors: false,
+}));
 
-export default startServer;
+app.listen({ port: constants.port }, () => {
+  console.log(`Server running on port: ${constants.port}`);
+});
