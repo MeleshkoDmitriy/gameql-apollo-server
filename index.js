2b91923a226384db51fe837ba4f292203c35f156
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-11-17 18:17:48 +0300

    vercel prepearings

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema.js";
-import { resolvers } from "./resolvers.js";
-import { constants } from "./constants.js";
+import { ApolloServer } from '@apollo/server';
+import { createHandler } from '@apollo/server/micro';
+import { microCors } from 'micro-cors';
+import { typeDefs } from './schema.js';
+import { resolvers } from './resolvers.js';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: constants.port },
-});
+const startServer = async () => {
+  const cors = microCors();
+  const handler = createHandler({ apolloServer: server });
 
-console.log("Server running on port:", constants.port);
-console.log("Server running on url:", url);
+  return cors(handler);
+};
 
+export default startServer;
